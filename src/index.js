fc1570300e9308b4e4108395ab5ccb91c3bd3eb7
palanik <palani.kumanan@gmail.com>
2017-02-09 19:34:33 -0500

    support for params

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import pathToRegexp from 'path-to-regexp';
+
 export default
 function redirector(...args) {
   const path = (args.length < 2) ? args[0] : args[1];
   const status = (args.length > 1) ? args[0] : 302;
 
   return function redirect(req, res) {
-    res.redirect(status, path);
+    res.redirect(status, pathToRegexp.compile(path)(req.params));
   };
 }
 
